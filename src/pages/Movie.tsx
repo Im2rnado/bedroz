f2e3a8aa389a513b0b6e1eb768dfc367c9ec7f35
im2rnado <yassin.bedier@gmail.com>
2023-06-06 22:55:22 +0300

    remove ads 100% and fix small issue in movie page

--- src/pages/Movie.tsx
+++ src/pages/Movie.tsx
@@ -1,5 +1,5 @@
-import { Fragment, useEffect, useState } from "react"
-import { useNavigate, useParams, Link, Navigate } from "react-router-dom"
+import { Fragment, useEffect, useState } from "react";
+import { useNavigate, useParams, Link, Navigate } from "react-router-dom";
 
 import conf from "../config";
 
@@ -16,30 +16,33 @@ import PosterSection from "../components/PosterSection";
 import Loading from "./Loading";
 import { Helmet } from "react-helmet";
 
-export default function Movie(){
+export default function Movie() {
   const nav = useNavigate();
   const { id } = useParams();
 
-  const [data, setData] = useState<MovieProps|null>();
+  const [data, setData] = useState<MovieProps | null>();
   const [loaded, setLoaded] = useState<boolean>(false);
 
-  async function loadData(){
+  async function loadData() {
     setData(null);
     setLoaded(false);
 
-    const req = await fetch(`https://api.themoviedb.org/3/movie/${id}?api_key=${conf.API_KEY}&append_to_response=recommendations,images`);
+    const req = await fetch(
+      `https://api.themoviedb.org/3/movie/${id}?api_key=${conf.API_KEY}&append_to_response=recommendations,images`
+    );
     const res = await req.json();
 
-    if(res.success == false ){
+    if (res.success == false) {
       nav("/unavailable");
       return;
     }
 
-    const nData:MovieProps = res;
+    const nData: MovieProps = res;
 
-    nData.logo = nData.images.logos.find(i => i.iso_639_1 == "en");
+    nData.logo = nData.images.logos.find((i) => i.iso_639_1 == "en");
     await loadImg("https://image.tmdb.org/t/p/original" + nData.backdrop_path);
-    if (nData.logo) await loadImg("https://image.tmdb.org/t/p/w500" + nData.logo.file_path);
+    if (nData.logo)
+      await loadImg("https://image.tmdb.org/t/p/w500" + nData.logo.file_path);
 
     setData(res);
     setLoaded(true);
@@ -49,40 +52,48 @@ export default function Movie(){
     loadData();
   }, [id]);
 
-  if(!loaded){
+  if (!loaded) {
     return <Loading />;
   }
 
-  if(!data){
+  if (!data) {
     return <Navigate to="/unavailable" />;
   }
 
   return (
     <Fragment>
       <Helmet>
-        <title>{data.title} - {conf.SITE_TITLE}</title>
+        <title>
+          {data.title} - {conf.SITE_TITLE}
+        </title>
       </Helmet>
-      
-      <MediaBackground backdrop={"https://image.tmdb.org/t/p/original" + data.backdrop_path} />
-    
+
+      <MediaBackground
+        backdrop={"https://image.tmdb.org/t/p/original" + data.backdrop_path}
+      />
+
       <div className="media-content">
         <div className="media-logo">
-          <img src={(data.logo ? "https://image.tmdb.org/t/p/w500" + data.logo.file_path : "")} draggable="false" />
+          <img
+            src={
+              data.logo
+                ? "https://image.tmdb.org/t/p/w500" + data.logo.file_path
+                : ""
+            }
+            draggable="false"
+          />
         </div>
 
         <div className="media-main">
-          {
-            data.tagline &&
-            <p className="media-tagline">{data.tagline}</p>
-          }
+          {data.tagline && <p className="media-tagline">{data.tagline}</p>}
 
           <div className="media-meta">
             <div className="media-genres">
-              {
-                data.genres.length ?
-                data.genres.map((v, i) => <span key={i}>{v.name}</span>) :
+              {data.genres.length ? (
+                data.genres.map((v, i) => <span key={i}>{v.name}</span>)
+              ) : (
                 <span>Movie</span>
-              }
+              )}
             </div>
 
             <div className="media-details">
@@ -104,13 +115,16 @@ export default function Movie(){
         </div>
       </div>
 
-      <MediaTabs
-      tabs={[
-        {
-          title: "Suggested",
-          element: <PosterSection posters={data.recommendations.results} />
-        }
-      ]} />
+      {data.recommendations && (
+        <MediaTabs
+          tabs={[
+            {
+              title: "Suggested",
+              element: <PosterSection posters={data.recommendations.results} />,
+            },
+          ]}
+        />
+      )}
     </Fragment>
-  )
+  );
 }
--- src/pages/Player.tsx
+++ src/pages/Player.tsx
@@ -1,16 +1,16 @@
-import { useState, useEffect, Fragment } from "react";
+import { useState, useEffect, Fragment, IframeHTMLAttributes } from "react";
 import { Helmet } from "react-helmet";
-import { Link, useParams, useSearchParams } from "react-router-dom"
+import { Link, useParams, useSearchParams } from "react-router-dom";
 
 import conf from "../config";
 
-export default function Player(){
+export default function Player() {
   const { id } = useParams();
   const [s] = useSearchParams();
 
   const [loaded, setLoaded] = useState<boolean>(false);
 
-  const [type, setType] = useState<"tv"|"movie">();
+  const [type, setType] = useState<"tv" | "movie">();
 
   const [season, setSeason] = useState<number>();
   const [episode, setEpisode] = useState<number>();
@@ -18,72 +18,81 @@ export default function Player(){
   const [maxSeasons, setMaxSeasons] = useState<number>();
   const [maxEpisodes, setMaxEpisodes] = useState<number>();
 
-  function showNext(){
-    if(!season || !episode || !maxSeasons || !maxEpisodes){
+  function showNext() {
+    if (!season || !episode || !maxSeasons || !maxEpisodes) {
       return false;
     }
 
-    if(season < maxSeasons || episode < maxEpisodes){
+    if (season < maxSeasons || episode < maxEpisodes) {
       return true;
     }
 
     return false;
   }
 
-  function getNext(){
-    if(!season || !episode || !maxSeasons || !maxEpisodes){
+  function getNext() {
+    if (!season || !episode || !maxSeasons || !maxEpisodes) {
       return "";
     }
 
-    if(episode < maxEpisodes){
-      return `?s=${season}&e=${episode+1}&ms=${maxSeasons}&me=${maxEpisodes}`;
+    if (episode < maxEpisodes) {
+      return `?s=${season}&e=${episode + 1}&ms=${maxSeasons}&me=${maxEpisodes}`;
     }
 
-    if(season < maxSeasons){
-      return `?s=${season+1}&e=1&ms=${maxSeasons}&me=${maxEpisodes}`;
+    if (season < maxSeasons) {
+      return `?s=${season + 1}&e=1&ms=${maxSeasons}&me=${maxEpisodes}`;
     }
 
     return "";
   }
 
-  function onNextClick(){
+  function removeAds(iframes: HTMLCollectionOf<HTMLIFrameElement>): any {
+    for (const iframe of iframes) {
+      iframe.setAttribute(
+        "sandbox",
+        "allow-forms allow-pointer-lock allow-same-origin allow-scripts allow-top-navigation"
+      );
+      console.log("Done for: " + iframe.src);
+    }
+  }
+
+  function onNextClick() {
     setType(undefined);
   }
 
   useEffect(() => {
     setLoaded(false);
 
-    if(s.has("s") && s.has("e")){
+    if (s.has("s") && s.has("e")) {
       let nSeason = parseInt(s.get("s")!);
       let nEpisode = parseInt(s.get("e")!);
 
-      if(!nSeason || !nEpisode){
+      if (!nSeason || !nEpisode) {
         return;
       }
 
-      if(nSeason < 1) nSeason = 1;
-      if(nEpisode < 1) nEpisode = 1;
-      
+      if (nSeason < 1) nSeason = 1;
+      if (nEpisode < 1) nEpisode = 1;
+
       setType("tv");
       setSeason(nSeason);
       setEpisode(nEpisode);
 
-      if(s.has("ms") && s.has("me")){
+      if (s.has("ms") && s.has("me")) {
         let mSeasons = parseInt(s.get("ms")!);
         let mEpisodes = parseInt(s.get("me")!);
-  
-        if(!mSeasons || !mEpisodes){
+
+        if (!mSeasons || !mEpisodes) {
           return;
         }
-  
-        if(mSeasons < 1) mSeasons = 1;
-        if(mEpisodes < 1) mEpisodes = 1;
+
+        if (mSeasons < 1) mSeasons = 1;
+        if (mEpisodes < 1) mEpisodes = 1;
 
         setMaxSeasons(mSeasons);
         setMaxEpisodes(mEpisodes);
       }
-    }
-    else{
+    } else {
       setType("movie");
     }
   }, [id, s]);
@@ -94,55 +103,49 @@ export default function Player(){
         <title>Player - {conf.SITE_TITLE}</title>
       </Helmet>
 
-      {
-        !loaded && 
+      {!loaded && (
         <div className="loading">
           <div className="spinner">
             <i className="fa-solid fa-spinner-third"></i>
           </div>
         </div>
-      }
+      )}
 
       <div className="player">
-        {
-          typeof type !== "undefined" &&
+        {typeof type !== "undefined" && (
           <iframe
-          allowFullScreen
-          onLoad={() => setLoaded(true)}
-          // sandbox = "allow-forms allow-pointer-lock allow-same-origin allow-scripts allow-top-navigation"
-          src={`https://vidsrc.me/embed/${id}${season ? "/"+season : ""}${episode ? "-"+episode : ""}`}></iframe>
-        }
+            allowFullScreen
+            onLoad={() => setLoaded(true)}
+            // sandbox="allow-forms allow-pointer-lock allow-same-origin allow-scripts allow-top-navigation"
+            src={`https://vidsrc.me/embed/${id}${season ? "/" + season : ""}${
+              episode ? "-" + episode : ""
+            }`}
+          ></iframe>
+        )}
 
-        {
-          loaded && 
-	<script>
-            {`
-	       var iframe = document.querySelector('player_iframe');
-	       iframe.sandbox = "allow-forms allow-pointer-lock allow-same-origin allow-scripts allow-top-navigation";
-            `}
-          </script>
-         }
+        {loaded && removeAds(document.getElementsByTagName("iframe"))}
 
-        {
-          loaded && 
+        {loaded && (
           <div className="overlay">
             <Link to="/">
               <i className="fa-solid fa-home"></i>
             </Link>
 
             {
-              (type && type === "tv" && showNext()) &&
-              <Link to={getNext()} onClick={() => onNextClick()}>
-                <i className="fa-solid fa-forward-step"></i>
-              </Link>
+              (type && type === "tv" && showNext() && (
+                <Link to={getNext()} onClick={() => onNextClick()}>
+                  <i className="fa-solid fa-forward-step"></i>
+                </Link>
+              ),
+              (
+                <Link to={`/${type}/${id}`}>
+                  <i className="fa-solid fa-close"></i>
+                </Link>
+              ))
             }
-
-            <Link to={`/${type}/${id}`}>
-                 <i className="fa-solid fa-close"></i>
-              </Link>
           </div>
-        }
+        )}
       </div>
     </Fragment>
-  ) 
+  );
 }
